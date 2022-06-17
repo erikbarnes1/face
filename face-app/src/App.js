61cb6baa8b3c97dee05f699c41166602e031a8dd
Erik <erikbarnes21@gmail.com>
2022-06-16 20:19:33 -0700

    second

--- face-app/src/App.js
+++ face-app/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import ImageLinkForm from './components/ImageLinkForm';
+import Rank from './components/Rank';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
-  );
+      );
 }
 
 export default App;
